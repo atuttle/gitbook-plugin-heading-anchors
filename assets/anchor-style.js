d9580193154cdc5cec6475f761f26bf19f1899d8
Adam Tuttle <adamtuttlecodes@gmail.com>
2016-01-26 09:54:32 -0500

    resolves #2: reload anchor links after page change

--- assets/anchor-style.js
+++ assets/anchor-style.js
@@ -1,5 +1,10 @@
 anchors.options = {
-	icon: '#',
-	placement: 'right'
-};
+	icon: '#'
+	,placement: 'right'
+}
+
+gitbook.events.bind('page.change', function(){
+	anchors.add('h1,h2,h3,h4,h5')
+})
+
 anchors.add('h1,h2,h3,h4,h5')
